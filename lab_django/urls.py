e41b46dfee53be64707cf9721fd320999b0f9c25
Lucaci Andrei <andreilucaci@windowslive.com>
2017-01-21 13:41:45 +0200

    Added db sqlite file

--- lab_django/urls.py
+++ lab_django/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from lab.views import view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
